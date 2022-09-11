9d06acec5d168ad39f58524ffa02f6851459aa7e
Leon Fedotov <fedotov.leon@gmail.com>
2022-09-12 01:11:33 +0300

    update with fix for apprunner

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-provider "aws" {
-  alias  = "cloudfront-certificate"
-  region = "us-east-1"
+locals {
+  zones = {
+    "us-east-1"      = "Z01915732ZBZKC8D32TPT"
+    "us-east-2"      = "Z0224347AD7KVHMLOX31"
+    "us-west-2"      = "Z02243383FTQ64HJ5772Q"
+    "ap-northeast-1" = "Z08491812XW6IPYLR6CCA"
+    "eu-west-1"      = "Z087551914Z2PCAU0QHMW"
+  }
 }
--- route53.tf
+++ route53.tf
@@ -2,49 +2,31 @@ data "aws_route53_zone" "root_zone" {
   name = var.root_domain_name
 }
 
-resource "aws_route53_record" "root_cloudfront_record" {
+resource "aws_route53_record" "root_record" {
   zone_id = data.aws_route53_zone.root_zone.zone_id
   name    = var.root_domain_name
   type    = "A"
   alias {
     evaluate_target_health = false
-    name                   = aws_cloudfront_distribution.server_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.server_distribution.hosted_zone_id
+    name                   = aws_apprunner_custom_domain_association.apprunner_custom_domain.dns_target
+    zone_id                = local.zones[var.region] #lol :'( https://docs.aws.amazon.com/general/latest/gr/apprunner.html
   }
 }
 
-resource "aws_route53_record" "acm_val_records" {
-  for_each = {
-    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
+resource "aws_route53_record" "apprunner_acm_val_record0" {
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
+  name            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].name
+  records         = [tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].value]
   ttl             = 300
-  type            = each.value.type
+  type            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].type
   zone_id         = data.aws_route53_zone.root_zone.zone_id
 }
 
-resource "aws_route53_record" "acm_certificate_validation" {
-  zone_id         = data.aws_route53_zone.root_zone.zone_id
-  name            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].resource_record_name
-  type            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].resource_record_type
-  records         = [ tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[0].resource_record_value ]
+resource "aws_route53_record" "apprunner_acm_val_record1" {
   allow_overwrite = true
-  ttl             = 60
-}
-resource "aws_route53_record" "sub_cloudfront_record" {
-  zone_id = data.aws_route53_zone.root_zone.zone_id
-  name    = "www.${var.root_domain_name}"
-  type    = "A"
-  alias {
-    evaluate_target_health = false
-    name                   = aws_cloudfront_distribution.server_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.server_distribution.hosted_zone_id
-  }
+  name            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[1].name
+  records         = [tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[1].value]
+  ttl             = 300
+  type            = tolist(aws_apprunner_custom_domain_association.apprunner_custom_domain.certificate_validation_records)[1].type
+  zone_id         = data.aws_route53_zone.root_zone.zone_id
 }
